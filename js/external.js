f5934e742fcad3b00904f539f685370eb4c52b66
Kiley Hector <kiley.hector@gmail.com>
2022-09-27 14:55:12 -0400

    fixing exercises

---
+++ js/external.js
@@ -0,0 +1,86 @@
+"use strict";
+//
+// console.log ("Sanity Check");
+// alert("Hello World from External JS");
+// alert("welcome to my website");
+//
+// let userInput = prompt('What is your favorite color?');
+// console.log('The user said: ' + userInput) + alert('Great! ' + userInput + " is my favorite color too!");
+
+// alert ('Welcome Khajit has wares');
+//
+// let userInput = prompt('How long would you like to rent Hercules for?');
+// ("User entered" + userInput) + alert( 'For ' + userInput + ' days');
+//
+// let price = alert(('That will be ') + (userInput * 3) + (' Tube Steaks'));
+//
+// let userInput1 = prompt('How long would you like to rent Little Mermaid for?');
+// ("User entered" + userInput1) + alert( 'For ' + userInput1 + ' days');
+//
+// let price2 = alert(('That will be ') + (userInput1 * 3) + (' Scoomas'));
+//
+// let userInput2 = prompt('Why would you rent this? Ok, for how long?');
+// ("User Entered" + userInput2) + alert('For ' + userInput2 + ' days');
+//
+// let price3 = alert(("That will be ") + (userInput2 * 3) + (' Scheckels'));
+//
+// let price4 = alert(('Total is ') + (userInput * 3 + userInput1 * 3 + userInput2 * 3) * 3);
+
+// alert("Sold my Soul to work for the man");
+//
+//
+// let google = 400;
+// let UserInput1 = prompt('How much of my soul ');
+// let GoogleTot = (UserInput1 * google);
+// alert(GoogleTot);
+//
+// let amazon = 380;
+// let UserInput2 = prompt('Blood for Bezos? ');
+// let AmazonTot = (UserInput2 * amazon );
+// alert(AmazonTot);
+//
+// let facebook = 350;
+// let UserInput3 = prompt('How man Social Points? ');
+// let FacebookTotal = (UserInput3 * facebook);
+// alert(FacebookTotal);
+//
+// alert(("Highscore of ") + (GoogleTot + AmazonTot + FacebookTotal));
+
+alert('Welcome to the Thunderdome');
+
+let isClassFull = confirm('is the thunderdome full?');
+let hasScheduleConflict = confirm ('You got time for the thunder dome?');
+let canEnroll = !isClassFull && !hasScheduleConflict;
+alert ('Can Enroll: ' + canEnroll)
+
+//let username = "codeup";
+// let password = "notastrongpassword";
+// let char5 = password.length >= 5;
+// let noUserName = password.includes(username);
+// let not20 = username.length <= 20;
+// let noWhiteUser = username[0] === " ";
+// let noWhitePAss = password[0] === " ";
+// let lastIndexUsername = username.length - 1;
+// let lastIndexPassword = password.length - 1;
+// let noWhiteEndPass = password[lastIndexPassword] === " ";
+// let noWhiteEndUser = username[lastIndexUsername] === ' ';
+// let whiteSpace = false;
+//
+// if((noWhiteUser === true) || (noWhitePAss === true) || (noWhiteEndPass === true) || (noWhiteEndUser === true)){
+//     whiteSpace = true;
+// };
+// console.log("5 characters is " + char5)
+// console.log("password contains username " + noUserName)
+// console.log("username is less than 20 " + not20)
+// console.log("password and username contain white space " + whiteSpace)
+
+
+
+
+
+
+
+
+
+
+
