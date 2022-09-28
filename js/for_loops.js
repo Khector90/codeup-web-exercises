c3c90695dc8f17c401556f3e256cac9e807b3075
Kiley Hector <kiley.hector@gmail.com>
2022-09-28 14:40:44 -0400

    box lecture stuff

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,13 +1,17 @@
 "use strict";
+// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
 
-function showMultiplicationTable(num, range) {
-    for (let i = 1; i <= range; ++i) {
-        document.write(num + " * " + i + " = " + num * i + "<br>");
-    }
-}
-let num = 7;
-let range = 10;
-document.write("Number: " + num + "<br>");
-document.write("Range: " + range + "<br>");
-document.write("Multiplication table of " + num + "<br>");
-showMultiplicationTable(num, range);
+// function showMultiplicationTable(num, range) {
+//     for (let i = 1; i <= range; ++i) {
+//         document.write(num + " * " + i + " = " + num * i + "<br>");
+//     }
+// }
+// let num = 7;
+// let range = 10;
+// document.write("Number: " + num + "<br>");
+// document.write("Range: " + range + "<br>");
+// document.write("Multiplication table of " + num + "<br>");
+// showMultiplicationTable(num, range);
+
+// Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+Math.random();
