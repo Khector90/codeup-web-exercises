e96efd958b820cbb29350f5aa2ac6fbe23025b20
Kiley Hector <kiley.hector@gmail.com>
2022-10-20 11:14:46 -0400

    Forgot qoute when commiting prev work

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -15,3 +15,16 @@
 $('#header').click(function() {
         $(this).css('background-color', '#FF0');
 })
+
+$('p').dblclick(function() {
+    $(this).css('font-size', '18px');
+})
+
+$('li').hover(
+    function() {
+        $(this).css('color', 'red');
+    },
+    function() {
+        $(this).css('color', 'black');
+    }
+);
