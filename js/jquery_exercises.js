56a91fb4dc5f2e61947b8b4f8c9abc0b820815ae
Kiley Hector <kiley.hector@gmail.com>
2022-10-20 10:25:01 -0400

    changes header to highlight yellow

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -4,11 +4,14 @@
 //     alert( 'The DOM has finished loading!' );
 // });
 
-let header= $('#header').html();
-alert(header);
+// let header= $('#header').html();
+// alert(header);
 
 // $(".li").css('font-size', '20px');
 
-$(".head, paragraph, li").css('background-color', '#FF0')
+// $(".head, paragraph, li").css('background-color', '#FF0')
 
 
+$('#header').click(function() {
+        $(this).css('background-color', '#FF0');
+})
