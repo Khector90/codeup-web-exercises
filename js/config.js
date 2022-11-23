74e20b028552211615897855b8e8be2549c2dc73
Kiley Hector <kiley.hector@gmail.com>
2022-11-23 01:01:36 -0500

    pic ain't dispalying oh well

---
+++ js/config.js
@@ -0,0 +1,5 @@
+const config = {
+    NASA_API_KEY: 'https://api.nasa.gov/planetary/apod?api_key=hSwAd7m0E6hzV9zMR2Hs3DYbilEEHPh8jENM1pfp', // Save your NASA API key here
+}
+
+// <!--saving this here for now https://api.nasa.gov/planetary/apod?api_key=hSwAd7m0E6hzV9zMR2Hs3DYbilEEHPh8jENM1pfp-->
--- js/keys.js
+++ js/keys.js
@@ -4,3 +4,4 @@ const github ='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'
 
 
 const weatherKey = "7e6f315709794d4937e8c8a99eb36ffc"
+
